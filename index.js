3f0b995d6dd8b2a66854e74c4d4f8634b1fa6cbc
Tek Loon <tekloon.1991@gmail.com>
2019-07-18 14:12:50 +0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// This is timer producer
+const INTERVAL_DURATION = 60000;
+
+const quote_queue = 'quote_queue';
+
+const url = process.env.CLOUDAMQP_URL || "amqp://localhost";
+const amqp = require('amqplib');
+
+amqp.connect(url).then((connection) => {
+    connection.createChannel().then((channel) => {
+        channel.assertQueue(quote_queue, { durable: true});
+        setInterval(() => {
+            const message = "Let go get the quote";
+            channel.sendToQueue(quote_queue, message,  {
+                persistent: true
+            });
+        }, INTERVAL_DURATION)
+    });
+}).catch((err) => {
+    console.error('Connect amqp failed', err);
+})
