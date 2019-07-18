8147ea291bd34324242e68ffe5fbcc6e6370bb3e
Tek Loon <tekloon.1991@gmail.com>
2019-07-18 14:58:51 +0800

    Setup basic express and .env

--- index.js
+++ index.js
@@ -6,17 +6,20 @@ const quote_queue = 'quote_queue';
 const url = process.env.CLOUDAMQP_URL || "amqp://localhost";
 const amqp = require('amqplib');
 
-amqp.connect(url).then((connection) => {
-    connection.createChannel().then((channel) => {
-        channel.assertQueue(quote_queue, { durable: true});
-        setInterval(() => {
-            const message = "Let go get the quote";
-            console.log('send to queue', message);
-            channel.sendToQueue(quote_queue, Buffer.from(message),  {
-                persistent: true
-            });
-        }, INTERVAL_DURATION)
-    });
-}).catch((err) => {
-    console.error('Connect amqp failed', err);
-})
+exports.startTimer = () => {
+    amqp.connect(url).then((connection) => {
+        connection.createChannel().then((channel) => {
+            channel.assertQueue(quote_queue, { durable: true});
+            setInterval(() => {
+                const message = "Let go get the quote";
+                console.log('send to queue', message);
+                channel.sendToQueue(quote_queue, Buffer.from(message),  {
+                    persistent: true
+                });
+            }, INTERVAL_DURATION)
+        });
+    }).catch((err) => {
+        console.error('Connect amqp failed', err);
+    })
+}
+
---
+++ server.js
@@ -0,0 +1,14 @@
+// Import env file
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const { startTimer } = require('./index');
+app.get('/', (req, res) => {
+    res.send('Timer Service is running. No worries man');
+});
+
+app.listen(process.env.PORT || 4000, () => {
+    console.log('Server is runnning in port: ', process.env.PORT);
+    startTimer();
+});
