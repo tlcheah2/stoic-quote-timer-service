4710aa3bf78256d10c18d516e0e056e0f967270b
Tek Loon <tekloon.1991@gmail.com>
2019-07-18 14:43:45 +0800

    Added start scripts

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ amqp.connect(url).then((connection) => {
         channel.assertQueue(quote_queue, { durable: true});
         setInterval(() => {
             const message = "Let go get the quote";
-            channel.sendToQueue(quote_queue, message,  {
+            console.log('send to queue', message);
+            channel.sendToQueue(quote_queue, Buffer.from(message),  {
                 persistent: true
             });
         }, INTERVAL_DURATION)
