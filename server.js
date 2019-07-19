d0515243a4895db10accc2dbf13078d4eec39e78
Tek Loon <tekloon.1991@gmail.com>
2019-07-20 00:32:48 +0800

    Addded keep awake for heroku

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const { startTimer } = require('./index');
+const { keepAwake } = require('./src/util/heroku_util');
 app.get('/', (req, res) => {
     res.send('Timer Service is running. No worries man');
 });
@@ -11,4 +12,5 @@ app.get('/', (req, res) => {
 app.listen(process.env.PORT || 4000, () => {
     console.log('Server is runnning in port: ', process.env.PORT);
     startTimer();
+    keepAwake();
 });
