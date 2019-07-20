3cfc3bfbb1045acfda28f4b63c50bfe206a05a59
Tek Loon <tekloon.1991@gmail.com>
2019-07-20 21:38:38 +0800

    Remove durable queue

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // This is timer producer
 const INTERVAL_DURATION = 60000;
 
-const quote_queue = 'quote_queue';
 
 const url = process.env.CLOUDAMQP_URL || "amqp://localhost";
 const amqp = require('amqplib');
@@ -9,13 +8,16 @@ const amqp = require('amqplib');
 exports.startTimer = () => {
     amqp.connect(url).then((connection) => {
         connection.createChannel().then((channel) => {
-            channel.assertQueue(quote_queue, { durable: true});
+            const exchange = 'quote';
+            channel.assertExchange(exchange, 'direct', {durable: false});
+            // channel.assertQueue(quote_queue, { durable: true});
             setInterval(() => {
                 const message = "Let go get the quote";
                 console.log('send to queue', message);
-                channel.sendToQueue(quote_queue, Buffer.from(message),  {
-                    persistent: true
-                });
+                // channel.sendToQueue(quote_queue, Buffer.from(message),  {
+                //     persistent: true
+                // });
+                channel.publish(exchange, '', Buffer.from(message));
             }, INTERVAL_DURATION)
         });
     }).catch((err) => {
